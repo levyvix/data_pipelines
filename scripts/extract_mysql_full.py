91260a88f05beacbfddf41158f79a6eabe0ce8d4
levyvix <levy.vix@gmail.com>
2024-01-05 22:35:46 -0300

    mysql full e rest

---
+++ scripts/extract_mysql_full.py
@@ -0,0 +1,66 @@
+import pymysql
+import csv
+import boto3
+import configparser
+
+
+if __name__ == "__main__":
+    parser = configparser.ConfigParser()
+    parser.read("../pipeline.conf")
+
+    # mysql
+    hostname = parser.get("mysql_config", "hostname")
+    port = parser.get("mysql_config", "port")
+    username = parser.get("mysql_config", "username")
+    dbname = parser.get("mysql_config", "database")
+    password = parser.get("mysql_config", "password")
+
+    conn = pymysql.connect(
+        host = hostname,
+        user = username,
+        password = password,
+        db = dbname,
+        port = int(port)
+    )
+
+    if conn is None:
+        print("Error connecting to mysql")
+    else:
+        print("Connection established!")
+
+
+    m_query = "SELECT * FROM ORDERS"
+    local_filename = "order_extract.csv"
+
+    m_cursor = conn.cursor()
+    m_cursor.execute(m_query)
+    
+
+    results = m_cursor.fetchall()
+
+    with open(local_filename, 'w', newline='') as f:
+        csv_w = csv.writer(f, delimiter=';')
+        csv_w.writerow(['id', 'status', 'date'])
+        csv_w.writerows(results)
+
+    m_cursor.close()
+    conn.close()
+
+
+
+    # aws s3
+    access_key = parser.get("aws_boto_credentials", "access_key")
+    secret_key = parser.get("aws_boto_credentials", "secret_key")
+    bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+
+    s3 = boto3.client("s3", aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+
+    s3_file = local_filename
+
+    s3.upload_file(
+        local_filename,
+        bucket_name,
+        s3_file
+    )
+
---
+++ scripts/rest_api/extract_rest.py
@@ -0,0 +1,57 @@
+import requests
+
+import json
+import csv
+import boto3
+
+lat = 42.36
+lon = 71.05
+
+lat_log_params = {"lat": lat, "lon": lon}
+
+
+def fetch_data():
+    api_reponse = requests.get(
+        "http://api.open-notify.org/astros.json", params=lat_log_params
+    )
+
+    if api_reponse.status_code == 200:
+        for item in json.loads(api_reponse.content)["people"]:
+            yield [item.get("name"), item.get("craft")]
+
+
+# for response in json_respose['']
+
+export_file = "export_file.csv"
+with open(export_file, "w", newline="") as f:
+    csvw = csv.writer(f, delimiter=";")
+    csvw.writerow(["name", "craft"])
+    for d in fetch_data():
+        csvw.writerow(d)
+
+
+# to s3
+        
+import configparser
+
+parser = configparser.ConfigParser()
+
+parser.read('pipeline.conf')
+
+
+
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+
+s3 = boto3.client("s3", aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+
+s3_file = export_file
+
+s3.upload_file(
+    export_file,
+    bucket_name,
+    s3_file
+)
+
